fcaf0e628302ebee9f0f3bdac26626bbbd1eb96d
Dong Hee Kim <opklnm102@gmail.com>
2018-05-24 01:55:01 +0900

    initial commit

---
+++ retry-basic/src/main/java/me/dong/retrybasic/RetryBasicApplication.java
@@ -0,0 +1,12 @@
+package me.dong.retrybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetryBasicApplication.class, args);
+    }
+}
